fa83455375af707e332fe7b2b620c0e8485e41e8
Diken <dikenmaharjan10@gmail.com>
2024-02-20 01:00:30 +0545

    refactor: rename UserHashedPhoneNumbersDTO.kt to  UserPhoneNumbersDTO

--- src/main/kotlin/com/safehill/kclient/api/SafehillApiImpl.kt
+++ src/main/kotlin/com/safehill/kclient/api/SafehillApiImpl.kt
@@ -26,7 +26,7 @@ import com.safehill.kclient.api.dtos.SHUserIdentifiersDTO
 import com.safehill.kclient.api.dtos.SHUserInputDTO
 import com.safehill.kclient.api.dtos.SHUserSearchDTO
 import com.safehill.kclient.api.dtos.SHUserUpdateDTO
-import com.safehill.kclient.api.dtos.UserHashedPhoneNumbersDTO
+import com.safehill.kclient.api.dtos.UserPhoneNumbersDTO
 import com.safehill.kclient.api.serde.toIso8601String
 import com.safehill.kclient.models.SHAssetDescriptor
 import com.safehill.kclient.models.SHAssetDescriptorUploadState
@@ -258,7 +258,7 @@ class SafehillApiImpl(
             return mapOf()
         }
 
-        val getUsersRequestBody = UserHashedPhoneNumbersDTO(phoneNumbers = hashedPhoneNumbers)
+        val getUsersRequestBody = UserPhoneNumbersDTO(phoneNumbers = hashedPhoneNumbers)
         return "/users/retrieve/phone-number".httpPost()
             .header(mapOf("Authorization" to "Bearer $bearerToken"))
             .body(Json.encodeToString(getUsersRequestBody))
--- src/main/kotlin/com/safehill/kclient/api/dtos/UserPhoneNumbersDTO.kt
+++ src/main/kotlin/com/safehill/kclient/api/dtos/UserPhoneNumbersDTO.kt
@@ -3,6 +3,6 @@ package com.safehill.kclient.api.dtos
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UserHashedPhoneNumbersDTO(
+data class UserPhoneNumbersDTO(
     val phoneNumbers: List<String>
 )
