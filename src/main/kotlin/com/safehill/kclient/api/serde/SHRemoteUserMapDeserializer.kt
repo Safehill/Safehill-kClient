3ea151a04416465fe3162ea0d50ef8fe8b165254
Diken <dikenmaharjan10@gmail.com>
2024-02-20 01:27:37 +0545

    refactor: use HashedPhoneNumber typealias in SHRemoteUserMapDeserializer

--- src/main/kotlin/com/safehill/kclient/api/serde/SHRemoteUserMapDeserializer.kt
+++ src/main/kotlin/com/safehill/kclient/api/serde/SHRemoteUserMapDeserializer.kt
@@ -1,6 +1,7 @@
 package com.safehill.kclient.api.serde
 
 import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.safehill.kclient.api.dtos.HashedPhoneNumber
 import com.safehill.kclient.models.SHRemoteUser
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.builtins.MapSerializer
@@ -12,23 +13,23 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.decodeStructure
 import kotlinx.serialization.json.Json
 
-class SHRemoteUserMapDeserializer : ResponseDeserializable<Map<String, SHRemoteUser>> {
+class SHRemoteUserMapDeserializer : ResponseDeserializable<Map<HashedPhoneNumber, SHRemoteUser>> {
     private val mapSerializer = MapSerializer(
-        String.serializer(),
+        HashedPhoneNumber.serializer(),
         SHRemoteUser.serializer()
     )
 
-    override fun deserialize(content: String): Map<String, SHRemoteUser> {
+    override fun deserialize(content: String): Map<HashedPhoneNumber, SHRemoteUser> {
         return Json.decodeFromString(
-            object : DeserializationStrategy<Map<String, SHRemoteUser>> {
+            object : DeserializationStrategy<Map<HashedPhoneNumber, SHRemoteUser>> {
                 override val descriptor: SerialDescriptor =
-                    buildClassSerialDescriptor("Map<String, SHRemoteUser>") {
-                        element<Map<String, SHRemoteUser>>("result")
+                    buildClassSerialDescriptor("Map<HashedPhoneNumber, SHRemoteUser>") {
+                        element<Map<HashedPhoneNumber, SHRemoteUser>>("result")
                     }
 
-                override fun deserialize(decoder: Decoder): Map<String, SHRemoteUser> {
+                override fun deserialize(decoder: Decoder): Map<HashedPhoneNumber, SHRemoteUser> {
                     return decoder.decodeStructure(descriptor) {
-                        var map: Map<String, SHRemoteUser>? = null
+                        var map: Map<HashedPhoneNumber, SHRemoteUser>? = null
                         when (val index = decodeElementIndex(descriptor)) {
                             0 -> map = decodeSerializableElement(
                                 descriptor = mapSerializer.descriptor,
