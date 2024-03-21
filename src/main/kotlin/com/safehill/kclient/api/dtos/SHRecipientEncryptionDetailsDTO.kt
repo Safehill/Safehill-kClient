06d63d28f644560a7033807e3ddeccb0f6fa27c4
Diken <dikenmaharjan10@gmail.com>
2024-03-21 23:18:04 +0545

    chore: add description to SHRecipientEncryptionDetailsDTO parameters

--- src/main/kotlin/com/safehill/kclient/api/dtos/SHRecipientEncryptionDetailsDTO.kt
+++ src/main/kotlin/com/safehill/kclient/api/dtos/SHRecipientEncryptionDetailsDTO.kt
@@ -7,7 +7,7 @@ data class SHRecipientEncryptionDetailsDTO(
     val recipientUserIdentifier: String,
     val ephemeralPublicKey: String, // base64EncodedData with the ephemeral public part of the key used for the encryption
     val encryptedSecret: String, // base64EncodedData with the secret to decrypt the encrypted content in this group for this user
-    val secretPublicSignature: String, // base64EncodedData with the public signature of the user sending it
-    val senderPublicSignature: String
+    val secretPublicSignature: String, // base64EncodedData with the public signature used for the encryption of the secret
+    val senderPublicSignature: String // base64EncodedData with the public signature of the user sending it
 )
 
