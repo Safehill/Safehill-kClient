bcd9fd66b747b7988ec2cdc110c207f406a8a23b
Diken <dikenmaharjan10@gmail.com>
2024-04-17 20:50:27 +0545

    feat: add function to get symmetric key from ConversationThreadOutputDTO

--- src/main/kotlin/com/safehill/kclient/network/dtos/ConversationThreadOutputDTO.kt
+++ src/main/kotlin/com/safehill/kclient/network/dtos/ConversationThreadOutputDTO.kt
@@ -1,5 +1,10 @@
 package com.safehill.kclient.network.dtos
 
+import com.safehill.kclient.models.SHLocalUser
+import com.safehill.kcrypto.SHCypher
+import com.safehill.kcrypto.base64.decodeBase64
+import com.safehill.kcrypto.models.SHPublicKey
+import com.safehill.kcrypto.models.SHSymmetricKey
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -9,4 +14,15 @@ data class ConversationThreadOutputDTO(
     val membersPublicIdentifier: List<String>,
     val lastUpdatedAt: String,
     val encryptionDetails: RecipientEncryptionDetailsDTO // for the user making the request
-)
+) {
+    fun getSymmetricKey(currentUser: SHLocalUser) = SHSymmetricKey(
+        SHCypher.decrypt(
+            sealedMessage = this.encryptionDetails.toShareablePayload(),
+            encryptionKey = currentUser.shUser.key,
+            protocolSalt = currentUser.encryptionSalt,
+            signedBy = SHPublicKey.from(
+                this.encryptionDetails.senderPublicSignature.toByteArray().decodeBase64()
+            )
+        )
+    )
+}
