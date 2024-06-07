157c0a1f719a9051f045eec3b8116c30bfa6876b
Diken <dikenmaharjan10@gmail.com>
2024-06-07 15:02:42 +0545

    feat: use Instant to represent time in ConversationThreadOutputDTO.kt and UserMessageDTO

--- src/main/kotlin/com/safehill/kclient/models/dtos/ConversationThreadOutputDTO.kt
+++ src/main/kotlin/com/safehill/kclient/models/dtos/ConversationThreadOutputDTO.kt
@@ -1,18 +1,20 @@
 package com.safehill.kclient.models.dtos
 
+import com.safehill.kclient.models.serde.InstantSerializer
 import com.safehill.kclient.models.users.LocalUser
 import com.safehill.kcrypto.SafehillCypher
 import com.safehill.kcrypto.base64.decodeBase64
 import com.safehill.kcrypto.models.SafehillPublicKey
 import com.safehill.kcrypto.models.SymmetricKey
 import kotlinx.serialization.Serializable
+import java.time.Instant
 
 @Serializable
 data class ConversationThreadOutputDTO(
     val threadId: String,
     val name: String?,
     val membersPublicIdentifier: List<String>,
-    val lastUpdatedAt: String,
+    @Serializable(with = InstantSerializer::class) val lastUpdatedAt: Instant,
     val creatorPublicIdentifier: String,
     val createdAt: String,
     val encryptionDetails: RecipientEncryptionDetailsDTO // for the user making the request
