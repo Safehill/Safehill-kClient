166e3d60febe04e23e06233b875a74f7096364e7
Diken <dikenmaharjan10@gmail.com>
2024-06-12 22:11:54 +0545

    feat: retry socket connection in case of an error with an exponential decay with max delay of 8 seconds.

--- src/main/kotlin/com/safehill/kclient/network/WebSocketApi.kt
+++ src/main/kotlin/com/safehill/kclient/network/WebSocketApi.kt
@@ -7,16 +7,20 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
 import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.plugins.websocket.webSocket
 import io.ktor.client.request.parameter
 import io.ktor.http.HttpMethod
 import io.ktor.http.Url
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
 import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.serialization.json.Json
+import kotlin.coroutines.cancellation.CancellationException
+import kotlin.coroutines.coroutineContext
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Obtain [WebSocketApi]'s instance from configured [com.safehill.SafehillClient]
@@ -35,39 +39,52 @@ class WebSocketApi internal constructor(
         install(WebSockets)
     }
 
-    private suspend fun getSocketSession(
-        currentUser: LocalUser,
-        deviceId: String
-    ): DefaultClientWebSocketSession {
-        return httpClient.webSocketSession(
-            method = HttpMethod.Get,
-            host = socketUrl.host,
-            path = "ws/messages",
-            port = socketUrl.port,
-            block = {
-                this.headers["Authorization"] = "Bearer ${currentUser.authToken}"
-                parameter("deviceId", deviceId)
-            }
-        )
-    }
-
     suspend fun connectToSocket(
         currentUser: LocalUser,
         deviceId: String,
         onSocketData: (WebSocketMessage) -> Unit
     ) {
-        val session = getSocketSession(
-            currentUser = currentUser,
-            deviceId = deviceId
-        )
-        session.incoming.consumeEach { frame ->
-            if (frame is Frame.Text) {
-                println(frame.readText())
-                val socketData = Json.decodeFromString(
-                    deserializer = WebSocketMessageDeserializer,
-                    string = frame.readText()
-                )
-                onSocketData(socketData)
+        monitorSocketConnection {
+            httpClient.webSocket(
+                method = HttpMethod.Get,
+                host = socketUrl.host,
+                path = "ws/messages",
+                port = socketUrl.port,
+                request = {
+                    this.headers["Authorization"] = "Bearer ${currentUser.authToken}"
+                    parameter("deviceId", deviceId)
+                }
+            ) {
+                this.incoming.consumeEach { frame ->
+                    if (frame is Frame.Text) {
+                        println(frame.readText())
+                        val socketData = Json.decodeFromString(
+                            deserializer = WebSocketMessageDeserializer,
+                            string = frame.readText()
+                        )
+                        onSocketData(socketData)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * The [block] should not complete till there is an active socket connection.
+     * Connect to socket inside the [block] and suspend forever.
+     * The connection will be closed once the coroutine is cancelled.
+     * If any error occurs, the [block] is executed again with certain delay to re establish socket connection.
+     */
+    private suspend fun monitorSocketConnection(block: suspend () -> Unit) {
+        var retryDelay = 1
+        while (coroutineContext.isActive) {
+            try {
+                block()
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                delay(1.seconds * retryDelay)
+                retryDelay = minOf(MAX_RETRY_DELAY, retryDelay * 2)
             }
         }
     }
