4e36c185ddeda466e4afb20d5565f7ed183dc635
Diken <dikenmaharjan10@gmail.com>
2024-07-04 10:12:10 +0545

    feat: remove BC provider if it already exists

--- src/main/kotlin/com/safehill/SafehillClient.kt
+++ src/main/kotlin/com/safehill/SafehillClient.kt
@@ -69,6 +69,11 @@ class SafehillClient private constructor(
         }
 
         private fun setupBouncyCastle() {
+            // Android registers its own BC provider. As it might be outdated and might not include
+            // all needed ciphers, we substitute it with a known BC bundled in the app.
+            if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) != null) {
+                Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+            }
             Security.addProvider(BouncyCastleProvider())
         }
 
