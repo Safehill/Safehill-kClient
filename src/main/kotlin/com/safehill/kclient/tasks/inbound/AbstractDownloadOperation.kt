a1725b4481fa9689257dcc8a2b8ca910b871f98f
Diken <dikenmaharjan10@gmail.com>
2024-07-13 00:42:53 +0545

    refactor: break down processAssetsInDescriptors function into multiple function

--- src/main/kotlin/com/safehill/kclient/tasks/inbound/AbstractDownloadOperation.kt
+++ src/main/kotlin/com/safehill/kclient/tasks/inbound/AbstractDownloadOperation.kt
@@ -3,6 +3,8 @@ package com.safehill.kclient.tasks.inbound
 import com.safehill.kclient.models.assets.AssetDescriptor
 import com.safehill.kclient.models.assets.AssetQuality
 import com.safehill.kclient.models.assets.DecryptedAsset
+import com.safehill.kclient.models.assets.EncryptedAsset
+import com.safehill.kclient.models.assets.EncryptedAssetVersion
 import com.safehill.kclient.models.users.ServerUser
 import com.safehill.kclient.models.users.UserIdentifier
 import com.safehill.kclient.tasks.BackgroundTask
@@ -24,7 +26,7 @@ abstract class AbstractDownloadOperation : DownloadOperation, BackgroundTask {
 
             listeners.forEach { listener ->
                 listener.received(
-                    descriptors,
+                    assetDescriptors = descriptors,
                     referencingUsers = users
                 )
             }
@@ -33,7 +35,7 @@ abstract class AbstractDownloadOperation : DownloadOperation, BackgroundTask {
                 process(descriptors)
             }
         } catch (e: Exception) {
-            println(e.message)
+            println("Error in download operation:$e " + e.message)
         }
     }
 
@@ -47,34 +49,43 @@ abstract class AbstractDownloadOperation : DownloadOperation, BackgroundTask {
         )
 
         descriptors.forEach { descriptor ->
-            allUsersDict[descriptor.sharingInfo.sharedByUserIdentifier]?.let { senderUser ->
+            val senderUser = allUsersDict[descriptor.sharingInfo.sharedByUserIdentifier]
+            if (senderUser != null) {
                 assetsDict[descriptor.globalIdentifier]?.let { encryptedAsset ->
-                    encryptedAsset.encryptedVersions[quality]?.let { assetVersion ->
-                        val sharedSecret = ShareablePayload(
-                            ephemeralPublicKeyData = assetVersion.publicKeyData,
-                            ciphertext = assetVersion.encryptedSecret,
-                            signature = assetVersion.publicSignatureData,
-                            recipient = this.user
-                        )
-                        val decryptedData = this.user.decrypted(
-                            assetVersion.encryptedData,
-                            encryptedSecret = sharedSecret,
-                            protocolSalt = this.user.encryptionSalt,
-                            receivedFrom = senderUser
-                        )
-                        val decryptedAsset = DecryptedAsset(
-                            encryptedAsset.globalIdentifier,
-                            encryptedAsset.localIdentifier,
-                            encryptedAsset.creationDate,
-                            decryptedVersions = mapOf(quality to decryptedData)
-                        )
-
-                        listeners.forEach { it.fetched(decryptedAsset) }
-
-                        RemoteDownloadOperation.alreadyProcessed.add(descriptor.globalIdentifier)
-                    }
+                    val decryptedAsset = encryptedAsset.toDecryptedAsset(
+                        senderUser = senderUser
+                    )
+                    listeners.forEach { it.fetched(decryptedAsset) }
+                    RemoteDownloadOperation.alreadyProcessed.add(descriptor.globalIdentifier)
                 }
             }
         }
+    }
+
+    private fun EncryptedAssetVersion.toShareablePayload() = ShareablePayload(
+        ephemeralPublicKeyData = this.publicKeyData,
+        ciphertext = this.encryptedSecret,
+        signature = this.publicSignatureData,
+        recipient = user
+    )
+
+    private fun EncryptedAsset.toDecryptedAsset(
+        senderUser: ServerUser
+    ): DecryptedAsset {
+        val decryptedVersions = this.encryptedVersions.mapValues { (quality, encryptedVersion) ->
+            val sharedSecret = encryptedVersion.toShareablePayload()
+            user.decrypted(
+                data = encryptedVersion.encryptedData,
+                encryptedSecret = sharedSecret,
+                receivedFrom = senderUser,
+                protocolSalt = user.encryptionSalt
+            )
+        }
+        return DecryptedAsset(
+            globalIdentifier = this.globalIdentifier,
+            localIdentifier = this.localIdentifier,
+            creationDate = this.creationDate,
+            decryptedVersions = decryptedVersions
+        )
     }
 }
