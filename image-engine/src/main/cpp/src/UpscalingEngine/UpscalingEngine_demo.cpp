6bf15062e44a281710c3622052bb1d80b94edb66
Lucchetto <lucchetto.tie@live.com>
2024-08-02 23:32:09 +0200

    UpscalingEngine_demo: support batch processing
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- image-engine/src/main/cpp/src/UpscalingEngine/UpscalingEngine_demo.cpp
+++ image-engine/src/main/cpp/src/UpscalingEngine/UpscalingEngine_demo.cpp
@@ -1,8 +1,34 @@
 #include <cxxopts.hpp>
 #include <iostream>
+#include <filesystem>
 
 #include "ImageUtils/ImageUtils.h"
 #include "UpscalingEngine/UpscalingEngine.h"
+
+bool createDirectoryIfNotExists(const std::string& directoryPath) {
+    std::filesystem::path path(directoryPath);
+
+    if (std::filesystem::exists(path)) {
+        return true;
+    } else {
+        return std::filesystem::create_directories(path);
+    }
+}
+
+
+void processImage(
+    const std::filesystem::path &inputImagePath,
+    UpscalingEngine &engine,
+    std::filesystem::path &outputDir) {
+
+    auto inputMat = ImageUtils::loadImageAsRGBAMat(std::filesystem::absolute(inputImagePath).c_str());
+    auto outputMat = cv::Mat(inputMat.rows * engine.scale, inputMat.cols * engine.scale, inputMat.type());
+    const auto outputImagePath = std::filesystem::absolute(outputDir / inputImagePath.filename());
+
+    engine.upscaleImage(nullptr, nullptr, nullptr, inputMat, outputMat);
+
+    ImageUtils::writeImageRGBAMat(outputImagePath.c_str(), outputMat, ImageUtils::OutputFormat::PNG);
+}
 
 int main(int argc, char** argv) {
     cxxopts::Options options("UpscalingEngine_demo", "UpscalingEngine demo");
@@ -11,7 +37,7 @@ int main(int argc, char** argv) {
         ("i,input", "Input image file", cxxopts::value<std::string>())
         ("m,model", "Upscaling model file in MNN format", cxxopts::value<std::string>())
         ("s,scale", "The scaling factor of the model", cxxopts::value<uint32_t>())
-        ("o,output", "Output image file", cxxopts::value<std::string>())
+        ("o,output", "Output directory", cxxopts::value<std::string>())
         ("t,tile-size", "Tile size used in processing, specify 0 or negative to disable tiling", cxxopts::value<std::int32_t>())
         ("h,help", "Print usage")
     ;
@@ -25,16 +51,28 @@ int main(int argc, char** argv) {
     const auto input = result["input"].as<std::string>();
     const auto model = result["model"].as<std::string>();
     const auto scale = result["scale"].as<uint32_t>();
-    const auto output = result["output"].as<std::string>();
+    const auto outputDir = result["output"].as<std::string>();
     const auto tileSize = result["tile-size"].as<std::int32_t>();
 
-    auto inputMat = ImageUtils::loadImageAsRGBAMat(input.c_str());
-    auto outputMat = cv::Mat(inputMat.rows * scale, inputMat.cols * scale, inputMat.type());
     auto upscalingEngine = UpscalingEngine(model.c_str(), scale, tileSize, INT32_MAX);
 
-    upscalingEngine.upscaleImage(nullptr, nullptr, nullptr, inputMat, outputMat);
+    try {
+        if (!createDirectoryIfNotExists(outputDir)) {
+            std::cout<<"Failed to create output directory"<<std::endl;
+            return -1;
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cout<<"Failed to create output directory"<<std::endl;
+        return -1;
+    }
 
-    ImageUtils::writeImageRGBAMat(output.c_str(), outputMat, ImageUtils::OutputFormat::PNG);
+    std::filesystem::path directoryPath(input);
+    std::filesystem::path outputPath(outputDir);
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
+        if (entry.is_regular_file()) {
+            processImage(entry.path(), upscalingEngine, outputPath);
+        }
+    }
 
     return 0;
 }
