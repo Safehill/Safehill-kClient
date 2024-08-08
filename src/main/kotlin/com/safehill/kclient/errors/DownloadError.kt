cc9a46213f0fb1de0b078dca21c869386af41425
Diken <dikenmaharjan10@gmail.com>
2024-08-08 18:53:12 +0545

    feat: show high resolution image in ImageFullScreen.kt

--- src/main/kotlin/com/safehill/kclient/errors/DownloadError.kt
+++ src/main/kotlin/com/safehill/kclient/errors/DownloadError.kt
@@ -2,6 +2,7 @@ package com.safehill.kclient.errors
 
 import com.safehill.kclient.models.assets.AssetDescriptor
 import com.safehill.kclient.models.assets.AssetGlobalIdentifier
+import com.safehill.kclient.models.assets.AssetQuality
 
 sealed class DownloadError : Exception() {
     data class IsBlacklisted(
@@ -18,5 +19,10 @@ sealed class DownloadError : Exception() {
 
     data class AssetDescriptorNotFound(
         val globalIdentifier: AssetGlobalIdentifier
+    ) : DownloadError()
+
+    data class AssetQualityNotFound(
+        val identifier: AssetGlobalIdentifier,
+        val quality: AssetQuality
     ) : DownloadError()
 }
