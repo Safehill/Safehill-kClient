aa937bc18b57fb7ecb4e4240cc76417a641e367d
Diken <dikenmaharjan10@gmail.com>
2024-09-06 11:24:04 +0545

    refactor: use BaseApi functions to make api calls in ThreadApiImpl.kt

--- src/main/kotlin/com/safehill/kclient/network/api/thread/ThreadApiImpl.kt
+++ src/main/kotlin/com/safehill/kclient/network/api/thread/ThreadApiImpl.kt
@@ -1,7 +1,5 @@
 package com.safehill.kclient.network.api.thread
 
-import com.github.kittinunf.fuel.httpPost
-import com.github.kittinunf.fuel.serialization.responseObject
 import com.safehill.kclient.models.dtos.ConversationThreadOutputDTO
 import com.safehill.kclient.models.dtos.CreateOrUpdateThreadDTO
 import com.safehill.kclient.models.dtos.HashedPhoneNumber
@@ -10,12 +8,8 @@ import com.safehill.kclient.models.dtos.RetrieveThreadDTO
 import com.safehill.kclient.models.users.LocalUser
 import com.safehill.kclient.models.users.UserIdentifier
 import com.safehill.kclient.network.api.BaseApi
-import com.safehill.kclient.network.api.getOrElseOnSafehillError
-import com.safehill.kclient.network.api.getOrThrow
 import com.safehill.kclient.network.api.postRequestForObjectResponse
 import com.safehill.kclient.network.exceptions.SafehillError
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
 
 class ThreadApiImpl(override val requestor: LocalUser) : ThreadApi, BaseApi {
     override suspend fun listThreads(): List<ConversationThreadOutputDTO> {
@@ -42,51 +36,35 @@ class ThreadApiImpl(override val requestor: LocalUser) : ThreadApi, BaseApi {
         ).firstOrNull()
     }
 
-    @OptIn(ExperimentalSerializationApi::class)
     override suspend fun retrieveThread(threadId: String): ConversationThreadOutputDTO? {
-        val bearerToken = this.requestor.authToken ?: throw SafehillError.ClientError.Unauthorized
-
-        return "/threads/retrieve/$threadId".httpPost()
-            .header(mapOf("Authorization" to "Bearer $bearerToken"))
-            .responseObject(
-                ConversationThreadOutputDTO.serializer(),
-                Json {
-                    explicitNulls = false
-                    ignoreUnknownKeys = true
-                }
+        return runCatching {
+            postRequestForObjectResponse<Unit, ConversationThreadOutputDTO>(
+                endPoint = "/threads/retrieve/$threadId",
+                authenticationRequired = true,
+                request = null
             )
-            .getOrElseOnSafehillError {
-                if (it is SafehillError.ClientError.NotFound) {
-                    null
-                } else {
-                    throw it
-                }
+        }.recoverCatching {
+            if (it is SafehillError.ClientError.NotFound) {
+                null
+            } else {
+                throw it
             }
+        }.getOrThrow()
     }
 
-    @OptIn(ExperimentalSerializationApi::class)
     override suspend fun createOrUpdateThread(
         name: String?,
         recipientsEncryptionDetails: List<RecipientEncryptionDetailsDTO>
     ): ConversationThreadOutputDTO {
-        val bearerToken = this.requestor.authToken ?: throw SafehillError.ClientError.Unauthorized
-
         val request = CreateOrUpdateThreadDTO(
             name = name,
             recipients = recipientsEncryptionDetails
         )
-        val json = Json {
-            explicitNulls = false
-            ignoreUnknownKeys = true
-        }
-
-
-        return "/threads/upsert".httpPost()
-            .header(mapOf("Authorization" to "Bearer $bearerToken"))
-            .body(json.encodeToString(CreateOrUpdateThreadDTO.serializer(), request))
-            .responseObject<ConversationThreadOutputDTO>(json)
-            .getOrThrow()
-
+        return postRequestForObjectResponse(
+            endPoint = "/threads/upsert",
+            request = request,
+            authenticationRequired = true
+        )
     }
 
 }
