7547b4c044ade77de00aa5c84fe1404214256097
Diken <dikenmaharjan10@gmail.com>
2024-09-25 12:53:37 +0545

    feat: create ConversationThreadController.kt to provide encryption details to a user

---
+++ src/main/kotlin/com/safehill/kclient/controllers/ConversationThreadController.kt
@@ -0,0 +1,41 @@
+package com.safehill.kclient.controllers
+
+import com.safehill.kclient.models.interactions.InteractionAnchor
+import com.safehill.kclient.models.users.ServerUser
+import com.safehill.kclient.network.ServerProxy
+import com.safehill.kclient.util.runCatchingPreservingCancellationException
+
+class ConversationThreadController(
+    val serverProxy: ServerProxy,
+    val userInteractionController: UserInteractionController,
+    val encryptionDetailsController: EncryptionDetailsController
+) {
+
+    suspend fun provideEncryptionDetailToUsers(
+        users: List<ServerUser>,
+        threadId: String
+    ): Result<Unit> {
+        return runCatchingPreservingCancellationException {
+            val symmetricKey = userInteractionController.getSymmetricKey(
+                anchorId = threadId,
+                interactionAnchor = InteractionAnchor.THREAD
+            )
+            if (symmetricKey == null) {
+                throw UserInteractionController.InteractionErrors.MissingE2EEDetails(
+                    anchorId = threadId,
+                    anchor = InteractionAnchor.THREAD
+                )
+
+            } else {
+                serverProxy.provideEncryptionDetails(
+                    threadIdWithEncryptionDetails = mapOf(
+                        threadId to encryptionDetailsController.getRecipientEncryptionDetails(
+                            users = users,
+                            secretKey = symmetricKey
+                        )
+                    )
+                )
+            }
+        }
+    }
+}
