b18d2fe44d084412abc8650d4b3cbfaf432b4a92
Diken <dikenmaharjan10@gmail.com>
2025-02-19 18:38:57 +0545

    feat: Upgrade to Gradle 8.11.1 and update dependencies
    - Updated Gradle wrapper to version 8.11.1 in both the main app and Safehill-kClient.
    - Upgraded `compileSdk` to 35.
    - Updated kotlin version to 2.1.10
    - Added a new `libs.versions.toml` file to the `Safehill-kClient` project to manage dependencies.
    - Migrated dependency declarations in `Safehill-kClient/build.gradle.kts` to use the `libs.versions.toml` file.
    - Updated dependency versions in the main `gradle/libs.versions.toml` file.
    - Updated android gradle plugin to 8.8.1
    - Updated ksp version to  2.1.0-1.0.29
    - Updated workRuntimeVersion to 2.10.0
    - Updated room to 2.7.0-beta01
    - Updated sentry to 5.1.0
    - Updated hilt to 2.53
    - Updated google-services to 4.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.serialization") version "1.9.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
     application
 }
 
@@ -13,25 +13,25 @@ repositories {
 }
 
 dependencies {
-    implementation("at.favre.lib:hkdf:2.0.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation(libs.hkdf)
+    implementation(libs.bcprov.jdk15on)
 
-    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:2.3.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("dev.turingcomplete:kotlin-onetimepassword:2.4.0")
+    implementation(libs.fuel)
+    implementation(libs.fuel.kotlinx.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlin.onetimepassword)
 
-    val ktor_version = "2.3.11"
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-logging:$ktor_version")
-    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.websockets)
+
+    implementation(kotlin("stdlib"))
 
     testImplementation(kotlin("test"))
-    testImplementation("org.mockito:mockito-core:4.8.1")
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.junit.jupiter)
 }
 
 tasks.test {
