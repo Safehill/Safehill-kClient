afba99a87035ec105814fde75c4a1a993bb171e0
Diken <dikenmaharjan10@gmail.com>
2025-03-03 11:23:32 +0545

    feat: add `contract` annotation to enhance compiler understanding in runCatchingPreservingCancellationException

--- src/main/kotlin/com/safehill/kclient/util/Utils.kt
+++ src/main/kotlin/com/safehill/kclient/util/Utils.kt
@@ -2,11 +2,17 @@ package com.safehill.kclient.util
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 import kotlin.coroutines.cancellation.CancellationException
 
+@OptIn(ExperimentalContracts::class)
 inline fun <T> runCatchingPreservingCancellationException(
     block: () -> T
 ): Result<T> {
+    contract {
+        callsInPlace(block, kotlin.contracts.InvocationKind.EXACTLY_ONCE)
+    }
     return try {
         Result.success(block())
     } catch (e: CancellationException) {
