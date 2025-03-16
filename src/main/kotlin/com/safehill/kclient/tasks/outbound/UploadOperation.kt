c39bc64d22e2b227e0dfe6a9b176228fc1871de5
Diken <dikenmaharjan10@gmail.com>
2025-03-16 12:50:01 +0545

    refactor: run code format in UploadOperation.kt

--- src/main/kotlin/com/safehill/kclient/tasks/outbound/UploadOperation.kt
+++ src/main/kotlin/com/safehill/kclient/tasks/outbound/UploadOperation.kt
@@ -8,17 +8,29 @@ import com.safehill.kclient.models.users.LocalUser
 import com.safehill.kclient.models.users.ServerUser
 import com.safehill.kclient.tasks.BackgroundTask
 
-interface UploadOperation: BackgroundTask {
+interface UploadOperation : BackgroundTask {
 
     val listeners: MutableList<UploadOperationListener>
 
     val user: LocalUser
 
-     fun enqueueUpload(localAsset: LocalAsset, assetQualities: Array<AssetQuality> = AssetQuality.entries.toTypedArray(), groupId: GroupId, recipients: List<ServerUser> = listOf(), uri: String? = null,
-                              threadId: String? = null)
+    fun enqueueUpload(
+        localAsset: LocalAsset,
+        assetQualities: Array<AssetQuality> = AssetQuality.entries.toTypedArray(),
+        groupId: GroupId,
+        recipients: List<ServerUser> = listOf(),
+        uri: String? = null,
+        threadId: String? = null
+    )
 
-    fun enqueueShare(localAsset: LocalAsset, assetQualities: Array<AssetQuality>, globalIdentifier: AssetGlobalIdentifier, groupId: GroupId, recipients: List<ServerUser>,
-                     threadId: String? = null)
+    fun enqueueShare(
+        localAsset: LocalAsset,
+        assetQualities: Array<AssetQuality>,
+        globalIdentifier: AssetGlobalIdentifier,
+        groupId: GroupId,
+        recipients: List<ServerUser>,
+        threadId: String? = null
+    )
 
     fun stop()
 }
